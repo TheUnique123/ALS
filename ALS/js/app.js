46291a60d3ac56e42d27e8b1c7d9ff7e42f79e60
TheUnique123 <165272684+TheUnique123@users.noreply.github.com>
2024-05-18 10:27:56 +0630

    FInal update

--- ALS/js/app.js
+++ ALS/js/app.js
@@ -4,6 +4,12 @@ const mimboxall = document.querySelectorAll(".mim_box");
 const fa_paw_am = document.querySelectorAll(".fa_paw_am");
 const fa_paw_am2 = document.querySelectorAll(".fa_paw_am2");
 const payment_inputs = document.querySelectorAll(".payment_input");
+const pay_logo_mother = document.querySelectorAll(".pay_logo_mother");
+const product_disables = document.querySelectorAll(".product_disable");
+const product_types = document.querySelectorAll(".product_type");
+const product_amount = document.querySelectorAll(".product_amount");
+const btn_product = document.querySelectorAll(".btn_product");
+const buy_now = document.querySelector(".buy_now");
 const amount_area = document.querySelector(".amount_area");
 const payment_area = document.querySelector(".payment_area");
 const gift_area = document.querySelector(".gift_area");
@@ -14,9 +20,13 @@ const amount_type = document.querySelector(".amount_type");
 const donated_times = document.querySelector(".donated_times");
 const donated_amount = document.querySelector(".donated_amount");
 const donate_btn = document.querySelector(".donate_btn");
+const modal_body = document.querySelector(".modal-body");
+const buy_confirm = document.querySelector(".buy_confirm");
+const close_buy = document.querySelector(".close_buy");
+const thank_for_buying = document.querySelector(".thank_for_buying");
 const pwc_main = document.querySelector(".pwc_main");
 const choose_btn = document.querySelector(".choose_btn");
-const pay_logo_mother = document.querySelectorAll(".pay_logo_mother");
+const shop_amount = document.querySelector(".shop_amount");
 
 // conditional statements
 let unselect = null;
@@ -26,6 +36,7 @@ let paw_am2 = null;
 let amount = null;
 let donate_option = null;
 let check_array = [];
+let shop_items = [];
 let amount_array = [15, 25, 50, 150, 250, 300, 500];
 
 // Landing Page JS 
@@ -35,6 +46,16 @@ function foodbowlchange(){
 function foodbowlchangeout(){
 document.getElementById('foodbowlhover').src ="images/foodbowl.png";
 }
+
+// active local storages
+if(!localStorage.getItem("donated times") && !localStorage.getItem("donated amount")){
+  localStorage.setItem("donated times", 0);
+  localStorage.setItem("donated amount", 0);
+  localStorage.setItem("donator", JSON.stringify([]));
+}
+
+donated_times.innerText = localStorage.getItem("donated times");
+donated_amount.innerText = localStorage.getItem("donated amount");
 
 // when select an amount this functin will run
 const slected = (ambox, paw, amt) =>{
@@ -53,32 +74,6 @@ const slected = (ambox, paw, amt) =>{
   paw_am = paw;
 };
 
-// when gift done
-const done =() =>{
-  alert("You've chosen, Thanks for donating" );
-  gift_area.style.display = "none";
-  amount_area.style.display = "unset"
-  unselect.style.backgroundColor = "white";
-  unselect.style.color = "black";
-  paw_am.style.display = "none";
-
-  unselect2.style.backgroundColor = "white";
-  unselect2.style.color = "black";
-  paw_am2.style.display = "none";
-
-  payment_inputs.forEach((input) => {
-    input.value = "";
-  })
-
-  unselect = null;
-  unselect2 = null;
-  paw_am = null;
-  paw_am2 = null;
-  amount = null;
-  donate_option = null;
-  
-}
-
 // when choose an option of donatin method this function will run
 const slected2 = (mimbox, paw2, option) =>{
   if (unselect2){
@@ -96,76 +91,8 @@ const slected2 = (mimbox, paw2, option) =>{
   paw_am2 = paw2;
 }
 
-// attaching with onClick to all " am box " elements
-amboxall.forEach((box, i) => {
-  box.addEventListener("click", () => {
-    slected(amboxall[i], fa_paw_am[i], amount_array[i]);
-  })
-});
-
-// active local storages
-// localStorage.setItem("donated times", 0);
-// localStorage.setItem("donated amount", 0);
-// localStorage.setItem("donator", JSON.stringify([]));
-
-donated_times.innerText = localStorage.getItem("donated times");
-donated_amount.innerText = localStorage.getItem("donated amount");
-
-
-
-
-
-// attaching with onClick to " amt box " element
-amt_box.addEventListener("click", () => {
-  if (unselect){
-    unselect.style.backgroundColor = "white";
-    unselect.style.color = "black";
-  }
-  if (paw_am){
-    paw_am.style.display = "none";
-  }
-  unselect = amt_box;
-  paw_am = null;
-  amt_box.style.backgroundColor = "black";
-  amt_box.style.color = "white";
-  amount_type.value = "$";
-});
-
-
-to_amount.addEventListener("click", () => {
-  payment_area.style.display = "none";
-  gift_area.style.display = "none";
-  amount_area.style.display = "unset";
-})
-
-to_payment.addEventListener("click", () => {
-  gift_area.style.display = "none";
-  payment_area.style.display = "unset";
-})
-// attaching with onClick to " mim box " element
-mimboxall[0].addEventListener("click", () =>{
-  slected2(mimboxall[0], fa_paw_am2[0], "Montly");
-});
-
-// attaching with onClick to " mim box " element
-mimboxall[1].addEventListener("click", () =>{
-  slected2(mimboxall[1], fa_paw_am2[1], "Once");
-});
-
-// attaching with onClick to " pay with card " element
-pwc_main.addEventListener("click", () => {
-  if ((!amount && !donate_option) || !amount || !donate_option){
-    alert("You have to choose amount and donate option first.");
-  }else{
-    amount_area.style.display = "none";
-    payment_area.style.display = "unset";
-  }
-});
-
-
-
-// attaching onClick to "donate_btn" element
-donate_btn.addEventListener("click", (e) => {
+//when donate button click this function will run
+const donate = (e) => {
   e.preventDefault();
 
   payment_inputs.forEach((input) => {
@@ -198,6 +125,139 @@ donate_btn.addEventListener("click", (e) => {
     gift_area.style.display = "unset";
   }
   check_array = [];
+}
+
+// when gift done
+const done =() =>{
+  alert("You've chosen, Thanks for donating" );
+  gift_area.style.display = "none";
+  amount_area.style.display = "unset"
+  unselect.style.backgroundColor = "white";
+  unselect.style.color = "black";
+  if(paw_am){
+    paw_am.style.display = "none";
+  }
+
+  unselect2.style.backgroundColor = "white";
+  unselect2.style.color = "black";
+  paw_am2.style.display = "none";
+  amount_type.value = "";
+
+  payment_inputs.forEach((input) => {
+    input.value = "";
+  })
+
+  unselect = null;
+  unselect2 = null;
+  paw_am = null;
+  paw_am2 = null;
+  amount = null;
+  donate_option = null;
+  
+}
+
+// attaching with onClick to all " am box " elements
+amboxall.forEach((box, i) => {
+  box.addEventListener("click", () => {
+    slected(amboxall[i], fa_paw_am[i], amount_array[i]);
+  })
+});
+
+// attaching with onClick to " amt box " element
+amt_box.addEventListener("click", () => {
+  if (unselect){
+    unselect.style.backgroundColor = "white";
+    unselect.style.color = "black";
+  }
+  if (paw_am){
+    paw_am.style.display = "none";
+  }
+  unselect = amt_box;
+  paw_am = null;
+  amt_box.style.backgroundColor = "black";
+  amt_box.style.color = "white";
+  amount_type.value = "$";
+});
+
+amount_type.addEventListener("change", () => {
+  amount = Number(amount_type.value.split("$")[1]);
+});
+
+// when click to shop this function will run
+const shopping = (i) => {
+  shop_items.push(i);
+  shop_amount.innerText = shop_items.length;
+  product_disables[i].style.display = "unset";
+}
+
+// when click "buy now" this function will run
+const buy_now_clicked = () => {
+  let total_amount = 0;
+  shop_items.forEach((item) => {
+    let type = product_types[item].innerText;
+    let the_amount = product_amount[item].innerText;
+    total_amount = total_amount + Number(product_amount[item].innerText.split("$")[1]);
+
+    let product_info = document.createElement("p");
+    product_info.innerText = type + " : " + the_amount;
+    modal_body.appendChild(product_info);
+  });
+  let the_total_amount = document.createElement("p");
+  the_total_amount.innerText = "total amount : $" + total_amount
+  modal_body.appendChild(the_total_amount);
+
+};
+
+const refresh_shop = (con) => {
+  let confirm = con;
+  shop_items = [];
+  shop_amount.innerText = shop_items.length;
+  modal_body.innerHTML = "";
+  if(confirm){
+    thank_for_buying.style.display = "unset";
+    setTimeout(() => {
+      thank_for_buying.style.display = "none";
+    }, 1000)
+  }
+  product_disables.forEach((disable) => {
+    disable.style.display = "none";
+  });
+}
+
+to_amount.addEventListener("click", () => {
+  payment_area.style.display = "none";
+  gift_area.style.display = "none";
+  amount_area.style.display = "unset";
+})
+
+to_payment.addEventListener("click", () => {
+  gift_area.style.display = "none";
+  payment_area.style.display = "unset";
+})
+// attaching with onClick to " mim box " element
+mimboxall[0].addEventListener("click", () =>{
+  slected2(mimboxall[0], fa_paw_am2[0], "Montly");
+});
+
+// attaching with onClick to " mim box " element
+mimboxall[1].addEventListener("click", () =>{
+  slected2(mimboxall[1], fa_paw_am2[1], "Once");
+});
+
+// attaching with onClick to " pay with card " element
+pwc_main.addEventListener("click", () => {
+  if ((!amount && !donate_option) || !amount || !donate_option){
+    alert("You have to choose amount and donate option first.");
+  }else{
+    amount_area.style.display = "none";
+    payment_area.style.display = "unset";
+  }
+});
+
+
+// attaching onClick to "donate_btn" element
+donate_btn.addEventListener("click", (e) => {
+  donate(e);
 });
 
 choose_btn.addEventListener("click", () => {
@@ -207,4 +267,22 @@ choose_btn.addEventListener("click", () => {
 pay_logo_mother[0].addEventListener("click", () => {
   amount_area.style.display = "none";
   gift_area.style.display = "unset";
+});
+
+btn_product.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    shopping(i);
+  })
+});
+
+buy_now.addEventListener("click", () => {
+  buy_now_clicked();
+});
+
+buy_confirm.addEventListener("click", () => {
+  refresh_shop(true);
+})
+
+close_buy.addEventListener("click", () => {
+  refresh_shop(false);
 })
